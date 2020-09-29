ba20cb63c151b60e82b1b5951f204d61b5012643
saef-rahal <55410380+saef-rahal@users.noreply.github.com>
2020-09-30 00:05:41 +0200

    The First Commit

---
+++ src/main/java/com/xzone/app/XzoneApplication.java
@@ -0,0 +1,13 @@
+package com.xzone.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xzone/app/XzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzone.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
