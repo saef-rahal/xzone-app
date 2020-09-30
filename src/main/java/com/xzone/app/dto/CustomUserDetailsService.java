171bb3bfba6d868263a2a2002feee712a2a6196c
saef-rahal <55410380+saef-rahal@users.noreply.github.com>
2020-09-30 07:40:37 +0200

    Add The User details Implementation

---
+++ src/main/java/com/xzone/app/dto/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.xzone.app.dto;
+
+import com.xzone.app.model.User;
+import com.xzone.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Created by Saef Rahal on 9/30/2020.
+ */
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailOrUsername(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException("User Not Found With Email Or Username"+usernameOrEmail));
+        return UserPrinciple.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(long id){
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User Not Found With ID: "+id));
+        return UserPrinciple.create(user);
+    }
+}
---
+++ src/main/java/com/xzone/app/dto/UserPrinciple.java
@@ -0,0 +1,83 @@
+package com.xzone.app.dto;
+
+import com.xzone.app.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Saef Rahal on 9/30/2020.
+ */
+public class UserPrinciple implements UserDetails {
+
+    private long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrinciple(long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserPrinciple create(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
+                new SimpleGrantedAuthority(role.getRoleName().name())
+        ).collect(Collectors.toList());
+
+        return new UserPrinciple(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
